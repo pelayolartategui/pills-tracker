1feb044dbb41b6de39a2585d96a81511e47faa83
plartategui <pelayo.lartategui@nexthink.com>
2022-01-12 22:33:24 +0100

    Adding fake user authenticated

--- src/main/java/com/pills/pillstracker/controllers/security/WebSecurityConfig.java
+++ src/main/java/com/pills/pillstracker/controllers/security/WebSecurityConfig.java
@@ -1,2 +1,54 @@
-package com.pills.pillstracker.controllers.security;public class WebSecurityConfig {
+package com.pills.pillstracker.controllers.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+
+import javax.sql.DataSource;
+
+
+@Configuration
+@EnableWebSecurity
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+
+    private final DataSource dataSource;
+
+    public WebSecurityConfig(DataSource dataSource) {
+
+        this.dataSource = dataSource;
+    }
+
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+            .authorizeRequests()
+            .antMatchers("/", "/home").permitAll()
+            .anyRequest().authenticated()
+            .and()
+            .formLogin()
+            .loginPage("/login")
+            .permitAll()
+            .and()
+            .logout()
+            .permitAll();
+    }
+
+    @Bean
+    @Override
+    public UserDetailsService userDetailsService() {
+        UserDetails user =
+            User.withDefaultPasswordEncoder()
+                .username("user")
+                .password("password")
+                .roles("USER")
+                .build();
+
+        return new InMemoryUserDetailsManager(user);
+    }
+
 }
