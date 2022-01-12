722bd354ec6d75f1233d1989009eb4fe6ead6514
plartategui <pelayo.lartategui@nexthink.com>
2022-01-12 19:54:45 +0100

    initial commit

---
+++ src/test/java/com/pills/pillstracker/PillsTrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pills.pillstracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PillsTrackerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
