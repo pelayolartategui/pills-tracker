b163defa6fd953e7e3ab7972a048789629e0f1ce
plartategui <pelayo.lartategui@nexthink.com>
2022-01-19 20:55:29 +0100

    Enabling develop actions

--- src/test/java/com/pills/pillstracker/PillsTrackerApplicationTests.java
+++ src/test/java/com/pills/pillstracker/PillsTrackerApplicationTests.java
@@ -1,15 +1,11 @@
 package com.pills.pillstracker;
 
-import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 
 @SpringBootTest
 class PillsTrackerApplicationTests {
 
-    @Test
-    void contextLoads() {
 
-    }
 
 }
