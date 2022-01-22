dff2fe3a47ab32bd7047b7c75101df9e6e16dbaa
plartategui <pelayo.lartategui@nexthink.com>
2022-01-22 21:20:39 +0100

    Configuring New Relic Java agent

---
+++ src/main/java/com/pills/pillstracker/metrics/NewRelicMetricsExportAutoConfiguration.java
@@ -0,0 +1,78 @@
+package com.pills.pillstracker.metrics;
+
+import com.newrelic.telemetry.Attributes;
+import com.newrelic.telemetry.micrometer.NewRelicRegistry;
+import com.newrelic.telemetry.micrometer.NewRelicRegistryConfig;
+import io.micrometer.core.instrument.config.MeterFilter;
+import io.micrometer.core.instrument.util.NamedThreadFactory;
+import org.springframework.boot.actuate.autoconfigure.metrics.CompositeMeterRegistryAutoConfiguration;
+import org.springframework.boot.actuate.autoconfigure.metrics.MetricsAutoConfiguration;
+import org.springframework.boot.actuate.autoconfigure.metrics.export.simple.SimpleMetricsExportAutoConfiguration;
+import org.springframework.boot.autoconfigure.AutoConfigureAfter;
+import org.springframework.boot.autoconfigure.AutoConfigureBefore;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+import java.time.Duration;
+
+
+@Configuration
+@AutoConfigureBefore({
+    CompositeMeterRegistryAutoConfiguration.class,
+    SimpleMetricsExportAutoConfiguration.class
+})
+@AutoConfigureAfter(MetricsAutoConfiguration.class)
+@ConditionalOnClass(NewRelicRegistry.class)
+public class NewRelicMetricsExportAutoConfiguration {
+
+    @Bean
+    public NewRelicRegistryConfig newRelicConfig() {
+
+        return new NewRelicRegistryConfig() {
+            @Override
+            public String get(String key) {
+
+                return null;
+            }
+
+            @Override
+            public String apiKey() {
+
+                return System.getenv("INSIGHTS_INSERT_KEY");
+            }
+
+            @Override
+            public Duration step() {
+
+                return Duration.ofSeconds(5);
+            }
+
+            @Override
+            public String serviceName() {
+
+                return "My Service Name";
+            }
+
+        };
+    }
+
+    @Bean
+    public NewRelicRegistry newRelicMeterRegistry(NewRelicRegistryConfig config)
+        throws UnknownHostException {
+
+        NewRelicRegistry newRelicRegistry =
+            NewRelicRegistry.builder(config)
+                .commonAttributes(
+                    new Attributes()
+                        .put("host", InetAddress.getLocalHost().getHostName()))
+                .build();
+        newRelicRegistry.config().meterFilter(MeterFilter.ignoreTags("plz_ignore_me"));
+        newRelicRegistry.config().meterFilter(MeterFilter.denyNameStartsWith("jvm.threads"));
+        newRelicRegistry.start(new NamedThreadFactory("newrelic.micrometer.registry"));
+        return newRelicRegistry;
+    }
+
+}
