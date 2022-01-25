d6f89393062cf0d14aca6fdc945eea7ed1eb44fb
plartategui <pelayo.lartategui@nexthink.com>
2022-01-25 21:59:05 +0100

    Fixing unit tests

--- src/test/java/com/pills/pillstracker/UserDtoUnitTest.java
+++ src/test/java/com/pills/pillstracker/UserDtoUnitTest.java
@@ -1,15 +1,22 @@
 package com.pills.pillstracker;
 
+import com.pills.pillstracker.config.H2TestProfileJPAConfig;
 import com.pills.pillstracker.models.daos.User;
 import com.pills.pillstracker.models.dtos.UserDto;
 import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
 import org.modelmapper.ModelMapper;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.junit4.SpringRunner;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 
-@SpringBootTest
+@RunWith(SpringRunner.class)
+@SpringBootTest(classes = {
+    H2TestProfileJPAConfig.class})
+@ActiveProfiles("test")
 public class UserDtoUnitTest {
 
     private final ModelMapper modelMapper = new ModelMapper();
